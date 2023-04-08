2ce112aece92133224ea3a1cb7f2501c42347ff7
husseinbouik <husseinbouik5@gmail.com>
2023-04-08 10:12:50 +0000

    plugin contact

--- wp-content/plugins/contact-form/contact-form.php
+++ wp-content/plugins/contact-form/contact-form.php
@@ -49,51 +49,200 @@ function contact_form_shortcode() {
         $email = sanitize_email( $_POST['email'] );
         $message = sanitize_textarea_field( $_POST['message'] );
         $date_sent = current_time( 'mysql' );
-        // Insert the form data into the database
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'contact_form';
-        $wpdb->insert(
-            $table_name,
-            array(
-                'subject' => $subject,
-                'first_name' => $first_name,
-                'last_name' => $last_name,
-                'email' => $email,
-                'message' => $message,
-                'date_sent' => $date_sent
-            )
-        );
-        // Display success message
-        $html .= '<div class="contact-form-message success">' . __( 'Thank you for your message!', 'contact-form' ) . '</div>';
-    }
-// Display the form
-$html .= '<form method="post" class="contact-form">';
-$html .= '<div class="form-group">';
-$html .= '<label for="subject">' . __( 'Subject', 'contact-form' ) . '</label>';
-$html .= '<input type="text" name="subject" id="subject" class="regular-text" required>';
+// Insert the form data into the database
+global $wpdb;
+$table_name = $wpdb->prefix . 'contact_form';
+$wpdb->insert( $table_name, array(
+    'subject' => $subject,
+    'first_name' => $first_name,
+    'last_name' => $last_name,
+    'email' => $email,
+    'message' => $message,
+    'date_sent' => $date_sent
+) );
+$html .= '<div class="notice notice-success is-dismissible">';
+$html .= '<p>' . __( 'Thank you for your message!', 'contact-form' ) . '</p>';
 $html .= '</div>';
-$html .= '<div class="form-group">';
-$html .= '<label for="first_name">' . __( 'First Name', 'contact-form' ) . '</label>';
-$html .= '<input type="text" name="first_name" id="first_name" class="regular-text" required>';
-$html .= '</div>';
-$html .= '<div class="form-group">';
-$html .= '<label for="last_name">' . __( 'Last Name', 'contact-form' ) . '</label>';
-$html .= '<input type="text" name="last_name" id="last_name" class="regular-text" required>';
-$html .= '</div>';
-$html .= '<div class="form-group">';
-$html .= '<label for="email">' . __( 'Email', 'contact-form' ) . '</label>';
-$html .= '<input type="email" name="email" id="email" class="regular-text" required>';
-$html .= '</div>';
-$html .= '<div class="form-group">';
-$html .= '<label for="message">' . __( 'Message', 'contact-form' ) . '</label>';
-$html .= '<textarea name="message" id="message" class="large-text" cols="30" rows="8" required></textarea>';
-$html .= '</div>';
-$html .= '<div class="form-group">';
-$html .= '<button type="submit" name="contact_form_submit" class="button button-primary">' . __( 'Submit', 'contact-form' ) . '</button>';
-$html .= '</div>';
-$html .= '</form>';
+// Check if the data was inserted successfully
+// if ( $wpdb->insert_id ) {
+//             // Display success message
+//     $html .= '<div class="notice notice-success is-dismissible"><p>' .
+//         __( 'Thank you for your message!', 'contact-form' ) .
+//         '</p></div>';
+// } else {
+//     $html .= '<div class="notice notice-error is-dismissible"><p>' .
+//         __( 'There was an error submitting your message. Please try again later.', 'contact-form' ) .
+//         '</p></div>';
+// }
+        }
+    // Display the form
+    $html .= '<form method="post" class="contact-form">';
+    $html .= '<div class="form-group">';
+    $html .= '<label for="subject">' . __( 'Subject', 'contact-form' ) . '</label>';
+    $html .= '<input type="text" name="subject" id="subject" class="regular-text" required>';
+    $html .= '</div>';
+    $html .= '<div class="form-group">';
+    $html .= '<label for="first_name">' . __( 'First Name', 'contact-form' ) . '</label>';
+    $html .= '<input type="text" name="first_name" id="first_name" class="regular-text" required>';
+    $html .= '</div>';
+    $html .= '<div class="form-group">';
+    $html .= '<label for="last_name">' . __( 'Last Name', 'contact-form' ) . '</label>';
+    $html .= '<input type="text" name="last_name" id="last_name" class="regular-text" required>';
+    $html .= '</div>';
+    $html .= '<div class="form-group">';
+    $html .= '<label for="email">' . __( 'Email', 'contact-form' ) . '</label>';
+    $html .= '<input type="email" name="email" id="email" class="regular-text" required>';
+    $html .= '</div>';
+    $html .= '<div class="form-group">';
+    $html .= '<label for="message">' . __( 'Message', 'contact-form' ) . '</label>';
+    $html .= '<input typr="text" name="message" id="message" class="large-text" cols="30" rows="8" required></input>';
+    $html .= '</div>';
+    $html .= '<div class="form-group">';
+    $html .= '<button type="submit" name="contact_form_submit" class="button button-primary">' . __( 'Submit', 'contact-form' ) . '</button>';
+    $html .= '</div>';
+    $html .= '</form>';
+    
+    // Add custom CSS
+    $html .= '<style>
+    body {
+        background: linear-gradient(45deg, #fc466b, #3f5efb);
+        height: 100vh;
+        font-family: "Montserrat", sans-serif;
+   }
+    .container {
+        position: absolute;
+        transform: translate(-50%, -50%);
+        top: 50%;
+        left: 50%;
+   }
+    form {
+        background: rgba(255, 255, 255, 0.3);
+        padding: 3em;
+        height: 320px;
+        border-radius: 20px;
+        border-left: 1px solid rgba(255, 255, 255, 0.3);
+        border-top: 1px solid rgba(255, 255, 255, 0.3);
+        backdrop-filter: blur(10px);
+        box-shadow: 20px 20px 40px -6px rgba(0, 0, 0, 0.2);
+        text-align: center;
+        position: relative;
+        transition: all 0.2s ease-in-out;
+   }
+    form p {
+        font-weight: 500;
+        color: #fff;
+        opacity: 0.7;
+        font-size: 1.4rem;
+        margin-top: 0;
+        margin-bottom: 60px;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+   }
+    form a {
+        text-decoration: none;
+        color: #ddd;
+        font-size: 12px;
+   }
+    form a:hover {
+        text-shadow: 2px 2px 6px #000 40;
+   }
+    form a:active {
+        text-shadow: none;
+   }
+    form input {
+        background: transparent;
+        width: 200px;
+        padding: 1em;
+        margin-bottom: 2em;
+        border: none;
+        border-left: 1px solid rgba(255, 255, 255, 0.3);
+        border-top: 1px solid rgba(255, 255, 255, 0.3);
+        border-radius: 5000px;
+        backdrop-filter: blur(5px);
+        box-shadow: 4px 4px 60px rgba(0, 0, 0, 0.2);
+        color: #fff;
+        font-family: Montserrat, sans-serif;
+        font-weight: 500;
+        transition: all 0.2s ease-in-out;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+   }
+    form input:hover {
+        background: rgba(255, 255, 255, 0.1);
+        box-shadow: 4px 4px 60px 8px rgba(0, 0, 0, 0.2);
+   }
+    form input[type="email"]:focus, form input[type="password"]:focus {
+        background: rgba(255, 255, 255, 0.1);
+        box-shadow: 4px 4px 60px 8px rgba(0, 0, 0, 0.2);
+   }
+    form input[type="button"] {
+        margin-top: 10px;
+        width: 150px;
+        font-size: 1rem;
+   }
+    form input[type="button"]:hover {
+        cursor: pointer;
+   }
+    form input[type="button"]:active {
+        background: rgba(255, 255, 255, 0.2);
+   }
+    form:hover {
+        margin: 4px;
+   }
+    ::placeholder {
+        font-family: Montserrat, sans-serif;
+        font-weight: 400;
+        color: #fff;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+   }
+    .drop {
+        background: rgba(255, 255, 255, 0.3);
+        backdrop-filter: blur(10px);
+        border-radius: 10px;
+        border-left: 1px solid rgba(255, 255, 255, 0.3);
+        border-top: 1px solid rgba(255, 255, 255, 0.3);
+        box-shadow: 10px 10px 60px -8px rgba(0, 0, 0, 0.2);
+        position: absolute;
+        transition: all 0.2s ease;
+   }
+    .drop-1 {
+        height: 80px;
+        width: 80px;
+        top: -20px;
+        left: -40px;
+        z-index: -1;
+   }
+    .drop-2 {
+        height: 80px;
+        width: 80px;
+        bottom: -30px;
+        right: -10px;
+   }
+    .drop-3 {
+        height: 100px;
+        width: 100px;
+        bottom: 120px;
+        right: -50px;
+        z-index: -1;
+   }
+    .drop-4 {
+        height: 120px;
+        width: 120px;
+        top: -60px;
+        right: -60px;
+   }
+    .drop-5 {
+        height: 60px;
+        width: 60px;
+        bottom: 170px;
+        left: 90px;
+        z-index: -1;
+   }
+    a, input:focus, select:focus, textarea:focus, button:focus {
+        outline: none;
+   }
+    ';    
 return $html;
 }
+
 // Add the menu page
 add_action( 'admin_menu', 'contact_form_menu' );
 function contact_form_menu() {
